c40d56c4a9a99717d5516a6589661fc735081112
tanakalucky <39301079+tanakalucky@users.noreply.github.com>
2023-11-13 11:34:31 +0000

    フッターコンポーネント追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from '@/components/Header';
 import { Memo } from '@/components/Memo';
+import { Footer } from '@/components/Footer';
 
 function App() {
   return (
     <>
       <Header />
       <Memo />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+export const Footer: FC = () => {
+  return (
+    <footer className="w-full flex items-center justify-center border-t border-blue-gray-50 py-6">
+      <p className="block font-sans text-base font-normal leading-relaxed text-blue-gray-900 antialiased">
+        © 2023 Mizuki Kawashima
+      </p>
+    </footer>
+  );
+};
